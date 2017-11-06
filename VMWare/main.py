cd568a38445394863558cd24610e956e0d97e0ca
Gregory Rebisz <grebisz@gmail.com>
2017-11-06 13:24:42 +0200

    Added config for VMWare

--- VMWare/main.py
+++ VMWare/main.py
@@ -1,12 +1,16 @@
 import ssl
+import json
 default_context = ssl._create_default_https_context
-
 from vmware import VMWare
 from api import API
+cfg = None
+with open('config.json') as json_data_file:
+    cfg = json.load(json_data_file)
+
 try:
     ssl._create_default_https_context = ssl._create_unverified_context
     #Establish Connection
-    vmware = VMWare("127.0.0.1:4433", "DS-HEADOFFICE\greg.rebisz", "m@tR1x@#$!@@")
+    vmware = VMWare(cfg["host"], cfg["username"], cfg["password"])
     api = API(vmware, 8080)
     #List Resources
     machines = vmware.getRegistered()
